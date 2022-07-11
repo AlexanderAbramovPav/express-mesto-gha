ae1d7e51a457d1df693cffb917374610eb778485
AlexanderAbramovPraktikum <95632162+AlexanderAbramovPraktikum@users.noreply.github.com>
2022-07-11 10:13:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true
-}, err => {
-  if(err) throw err;
-  console.log('Подключены MongoDB!')
+  useNewUrlParser: true,
+}, (err) => {
+  if (err) throw err;
 });
 
 // подключаем мидлвары, роуты и всё остальное
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '62cb1afe8a1263095cef79bd'
+    _id: '62cb1afe8a1263095cef79bd',
   };
 
   next();
@@ -27,6 +26,5 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-
 
 app.listen(3000);
